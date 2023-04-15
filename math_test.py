cb74eb0406fae663924b6649cee6846a02ad90ee
Joao Costa <joao.costa@pulses.com.br>
2023-04-14 23:20:11 -0300

    math operations

---
+++ math_test.py
@@ -0,0 +1,10 @@
+numberA = 10
+numberB = 20
+
+print(numberA + numberB)
+print(numberA - numberB)
+print(numberA * numberB)
+print(numberA / numberB)
+print(numberA // numberB)
+print(20 % 3)
+print(2 ** 3)
